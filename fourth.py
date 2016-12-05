f7d95d40e805f6edd13910447170d661b3509d69
Bencso <gabor.bencsik13@gmail.com>
2016-12-05 10:59:30 +0100

    feat(python exam): fourth task

--- fourth.py
+++ fourth.py
@@ -22,6 +22,33 @@
 
 # The following code should work with the class:
 
+class Rocket:
+
+    def __init__(self, rocket_type, fuel_level=0, launches=0):
+        self.rocket_type = rocket_type
+        self.fuel_level = fuel_level
+        self.launches = launches
+
+    def launch(self):
+        if self.rocket_type == "falcon1" and self.fuel_level > 1:
+            self.fuel_level -= 1
+        elif self.rocket_type == "falcon9" and self.fuel_level >9:
+            self.fuel_level -= 9
+        self.launches += 1
+
+    def refill(self):
+        if self.rocket_type == "falcon1" and self.fuel_level < 5:
+            refilled_fuel = 5 - self.fuel_level
+            self.fuel_level = 5
+            return refilled_fuel
+        elif self.rocket_type == "falcon1" and self.fuel_level < 20:
+            refilled_fuel = 20 - self.fuel_level
+            self.fuel_level = 20
+            return refilled_fuel
+
+    def getStats(self):
+        return "name: {}, fuel: {}, launches: {}".format(self.rocket_type, self.fuel_level, self.launches)
+
 falcon1 = Rocket('falcon1')
 returned_falcon9 = Rocket('falcon9', 11, 1)
 
