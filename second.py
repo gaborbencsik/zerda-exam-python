e381d28561c6ca9ec3ef5c5800b150deb1148813
Bencso <gabor.bencsik13@gmail.com>
2016-12-05 09:43:46 +0100

    feat(python exam): second task

--- second.py
+++ second.py
@@ -4,3 +4,15 @@
 # If any problem raises with the file output, the function should not break, but return False.
 # Example: when called with the following two parameters: "tree.txt", "apple",
 # the function should write "appleappleapple" to the file "tree.txt", and return True.
+
+def write_in_file(filename, word):
+    try:
+        f = open(filename, 'a')
+        f.write(10*word)
+        f.close()
+        success = True
+    except IOError:
+        success = False
+    return success
+
+print(write_in_file('text.txt', 'parameter '))
